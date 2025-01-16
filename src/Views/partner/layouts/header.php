6923a10f5d57073e25317ec07f20bdefd6b65f22
Diego Gonzalez Marignac <dfgonzalez@gmail.com>
2025-01-16 12:17:38 -0300

    Favicon

--- src/Views/partner/layouts/header.php
+++ src/Views/partner/layouts/header.php
@@ -9,6 +9,11 @@
     <title><?= htmlspecialchars($title ?? 'Partner Portal - Numok') ?></title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/alpinejs" defer></script>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/assets/favicon/site.webmanifest">
 </head>
 <body class="h-full">
     <?php if (isset($_SESSION['partner_id'])): ?>
